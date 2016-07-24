d1656600c048820cbab7b15610a33f6d8e59ae0f
Rangy Abines <rangyabines11@gmail.com>
2016-07-24 18:37:16 -0400

    updated files

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root", //Your username//
-    password: "Driven@nz198!", //Your password//
+    password: "ENTER PASSWORD", //Your password//
     database: "Bamazon"
 })
 
